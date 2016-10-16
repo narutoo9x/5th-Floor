7f26c6a1c20bfc333b065816fecf478a8babd939
Thanh Khac <kingkid162@gmail.com>
2016-10-16 10:40:43 +0700

    User can filter by section.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,15 @@
 class WelcomeController < ApplicationController
   def index
+    
   end
-
+  
   def menu
+    if params[:section]
+      @section = Section.where(name: params[:section]).first
+      @food_items = @section.food_items
+    else
+      @food_items = FoodItem.all
+    end
   end
 
   def contact_us
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
 
 	resources 'food_items'
-
 	root 'welcome#index'
-
 	get 'menu' => 'welcome#menu'
-
+	
 	get 'contact_us' => 'welcome#contact_us'
 
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
