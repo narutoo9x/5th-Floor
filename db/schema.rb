ad4488a2cc1640fe4494c432a094da97fdbd6bf8
Thanh Khac <kingkid162@gmail.com>
2016-10-16 21:20:29 +0700

    Order Orderitems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161016003020) do
+ActiveRecord::Schema.define(version: 20161016103933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,24 @@ ActiveRecord::Schema.define(version: 20161016003020) do
     t.index ["section_id"], name: "index_food_items_on_section_id", using: :btree
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.string   "quantity"
+    t.integer  "food_item_id"
+    t.integer  "order_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["food_item_id"], name: "index_order_items_on_food_item_id", using: :btree
+    t.index ["order_id"], name: "index_order_items_on_order_id", using: :btree
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "name"
+    t.text     "address"
+    t.datetime "confirm_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sections", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -34,4 +52,6 @@ ActiveRecord::Schema.define(version: 20161016003020) do
   end
 
   add_foreign_key "food_items", "sections"
+  add_foreign_key "order_items", "food_items"
+  add_foreign_key "order_items", "orders"
 end
